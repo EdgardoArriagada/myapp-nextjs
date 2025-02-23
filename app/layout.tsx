756cf2a6dfead042d33f78708219bb261cec17b5
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2025-02-23 20:32:17 -0300

    add page layout

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function RootLayout({ children }: Props) {
+  return (
+    <html>
+      <head>
+        <body>{children}</body>
+      </head>
+    </html>
+  );
+}
