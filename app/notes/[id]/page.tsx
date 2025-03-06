a453049b2f90fb6c2f07d9243f8dea9834c8d3b6
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2025-03-06 20:53:09 -0300

    force types

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,23 +1,29 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import PocketBase from "pocketbase";
 
-async function getNote(noteId: string) {
+type Note = {
+  id: string;
+  title: string;
+  content: string;
+  created: string;
+};
+
+async function getNote(noteId: string): Promise<Note> {
   const db = new PocketBase("http://127.0.0.1:8090");
   const data = await db.collection("notes").getOne(noteId);
 
-  return data?.items as any[];
+  return data as unknown as Note;
 }
 
-export default function NotePage({ params }: any) {
-  const note = await getNote(params.id);
+export default async function NotePage({ params }: { params: { id: string } }) {
+  const { id, title, content, created } = await getNote(params.id);
 
   return (
     <div>
-      <h1>notes/{note.id}</h1>
+      <h1>notes/{id}</h1>
       <div>
-        <h3>{note.title}</h3>
-        <h5>{note.content}</h5>
-        <p>{note.created}</p>
+        <h3>{title}</h3>
+        <h5>{content}</h5>
+        <p>{created}</p>
       </div>
     </div>
   );
